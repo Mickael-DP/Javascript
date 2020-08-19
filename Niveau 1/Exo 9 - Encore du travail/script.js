ea32ac65ad884760748b5816a157d2b46085bae9
Mickael-DP <67142286+Mickael-DP@users.noreply.github.com>
2020-08-19 11:55:04 +0200

    exercise finished

---
+++ Niveau 1/Exo 9 - Encore du travail/script.js
@@ -0,0 +1,9 @@
+var mine = 100;
+
+//Ecrivez ici la boucle qui vous permet d'enlever 1 unité d'or de la mine et d'annoncer
+//dans la console combien il reste d'or dans la mine.
+for (var i = 100; i > 0;){
+    i--;
+    console.log("il reste", i, "unité d'or de la mine");
+}
+    console.log("il ne reste plus d'or dans la mine")
